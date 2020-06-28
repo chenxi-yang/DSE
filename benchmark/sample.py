f96d7e4e8faeb6b68b9acdc25e8683c33c71ba81
cxyang <cxyang@cs.utexas.edu>
2020-06-28 00:46:52 -0500

    if-statement AI + AD update, for box domain

--- benchmark/sample.py
+++ benchmark/sample.py
@@ -4,6 +4,8 @@ import random
 
 interval_a, interval_b = -3, 10
 constraint_a, constraint_b = -20, 60
+a2 = -1
+b2 = 1
 
 def target_f(a, b):
     B, F = 130, 10
@@ -18,6 +20,7 @@ def target_f(a, b):
     f = torch.max(C1 * X1 + T + C2, C1 * X2 + T + C2)
 
     return f
+
 
 def cal_theta(lr, epoch):
 
@@ -37,10 +40,7 @@ def cal_theta(lr, epoch):
     # f = torch.max(torch.mul(C1, X1).add(torch.mul(C2, T)).add(C2), torch.mul(C1, X2).add(T).add(C2))
     # f = T.pow(2)
     for i in range(epoch):
-        # print(f)
-        # 5<=y<=60
-        # dT = torch.autograd.grad(torch.max(torch.max(L.sub(torch.mul(C1, X1).add(T).add(C2)), (torch.mul(C1, X2).add(T).add(C2)).sub(R)), C3), T)
-        # -20<=y<=60, y<=x^2
+        
         X = Variable(torch.tensor(random.uniform(interval_a, interval_b)))
         dT = torch.autograd.grad(torch.max(torch.max(torch.max(L.sub(torch.mul(C1, X1).add(T).add(C2)), (torch.mul(C1, X2).add(T).add(C2)).sub(R)), torch.mul(C1, X).add(T).add(C2).sub(torch.mul(C1, X))), C3), T)
         
@@ -50,12 +50,95 @@ def cal_theta(lr, epoch):
         T.data -= lr * derivation.data
     return T.data
 
+
+def select_interval(T):
+    
+    C1 = Variable(torch.tensor(-5, dtype=torch.float))
+    C2 = Variable(torch.tensor(9, dtype=torch.float))
+    C3 = Variable(torch.tensor(-1, dtype=torch.float))
+    C4 = Variable(torch.tensor(1, dtype=torch.float))
+
+    theta = T.data
+
+    if theta >= 1/11:
+        l = torch.mul(C1, T).add(T)
+        r = torch.mul(C2, T).add(T)
+    elif theta >= 0:
+        l = torch.mul(C1, T).add(T)
+        r = torch.mul(C3, T).add(C4)
+    elif theta > -1/3:
+        l = torch.mul(C2, T).add(T)
+        r = torch.mul(C3, T).add(C4)
+    elif theta > -6:
+        print('here4, T.data', T.data)
+        l = torch.mul(C2, T).add(T)
+        r = torch.mul(C1, T).add(T)
+        print('l-r', l.data, r.data)
+    else:
+        l = torch.mul(C3, T).add(C4)
+        r = torch.mul(C3, T).add(C4)
+    
+    return l, r
+
+
+def select_function(l, r, T):
+    C1 = Variable(torch.tensor(a2, dtype=torch.float))
+    C2 = Variable(torch.tensor(b2, dtype=torch.float))
+    C3 = Variable(torch.tensor(1, dtype=torch.float))
+    C4 = Variable(torch.tensor(0, dtype=torch.float))
+    epsilon = Variable(torch.tensor(0.0001, dtype=torch.float))
+    print(l.data, r.data)
+
+    if min(max(l.data - b2, a2 - r.data), 0) == 0:
+        print('empty')
+        f = C4.sub(torch.min(torch.abs(C2.sub(r)), torch.min(torch.abs(C1.sub(l)), torch.min(torch.abs(C1.sub(r)), torch.abs(l.sub(C2))))))
+    else:
+        if(r.data - b2 > 0):
+            if(l.data - a2) >= 0:
+                f = torch.min(C3, torch.div(torch.max(C2.sub(l), epsilon), torch.max(r.sub(l), epsilon)))
+            else:
+                # print('here, c < ai output')
+                # print('C2.sub(C1)', C2.sub(C1).data)
+                # print('r.sub(l))', r.sub(l).data)
+                f = torch.min(C3, torch.div(torch.max(C2.sub(C1), epsilon), torch.max(r.sub(l), epsilon)))
+        else:
+            if(l.data - a2 >= 0):
+                print('c in lr!!!')
+                f = torch.mul(C4, T)
+            else:
+                f = torch.min(C3, torch.div(r.sub(C1), torch.max(r.sub(l), epsilon)))
+    
+    return f
+
+    
+def ai_cal_theta(lr, epoch):
+
+    # T = Variable(torch.tensor(random.uniform(-10, 10)), requires_grad=True)
+    T = Variable(torch.tensor(random.uniform(-10, 10)), requires_grad=True)
+    print('T.data', T.data)
+
+    for i in range(epoch):
+        l, r = select_interval(T)
+        f = select_function(l, r, T)
+        print('f.data', f.data)
+
+        dT = torch.autograd.grad(f, T)
+        derivation = dT[0]
+        print('derivation', derivation)
+        if derivation == 0:
+            return T.data
+        T.data += lr * derivation.data
+        print('T.data', T.data)
+    
+    return T.data
+
 def sol():
-    lr = 0.1 # 1e-1
-    epoch = 1000
+    lr = 1.0 # 1e-1
+    epoch = 10000
     # target_func = target_f(interval_a, interval_b)
 
-    theta = cal_theta(lr, epoch)
+    # theta = cal_theta(lr, epoch)
+    theta = ai_cal_theta(lr, epoch)
     print(theta)
 
 sol()
--- benchmark/tool.py
+++ benchmark/tool.py
@@ -2,3 +2,124 @@ import sys
 sys.path.append('/Users/cxyang/anaconda3/lib/python3.7/site-packages/pydrogen')
 
 import pydrogen
+from typing import Any, List, Callable, TypeVar, NewType, Tuple
+#interval
+
+class Interval(object):
+    """
+    A half-open interval on the real number line.
+    """
+    def __init__(self, lo, hi):
+        self.lo = lo
+        self.hi = hi
+
+    def __repr__(self):
+        return 'Interval(%f, %f)' % (self.lo, self.hi)
+
+    def width(self):
+        return self.hi - self.lo
+
+
+class Solution:
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+
+        out = []  
+        for i in sorted(intervals, key=lambda i: i.start):
+            if out and i.start <= out[-1].end:
+                out[-1].end = max(out[-1].end, i.end)
+            else:
+                out.append(i)
+        return out
+
+class IntervalAI(pydrogen.Typical):
+    def Statements(self, ss): return ss.post()[-1] # Last statement.
+    def Module(self, ss): return ss.post()
+    def FunctionDef(self, ss): return ss.post()
+    def Return(self, e): return e.post()
+
+    # def True_(self): return 'Bool'
+    # def False_(self): return 'Bool'
+    # def BoolOp(self, es): return 'Bool'if frozenset(es.post()) == {'Bool'} else 'Error'
+    # def Not(self, e): return 'Bool' if e.post() == 'Bool' else 'Error'
+
+    def Assign(self, targets, e, context=None): 
+        # print(dir(targets), dir(e))
+        print('assign', e.post(context))
+        return e.post(context)
+    def Tuple(self, es): return es
+    def Num(self, n): 
+        print('n', n.post())
+        return n.post() # return 'Int'
+    def Add(self, e1, e2): 
+        print('here')
+        print(e1.post(), e2.post())
+        if type(e1.post()) == Tuple and type(e2.post()) == Tuple:
+            # print('here1')
+            return Tuple(e1.post()[0] + e2.post()[0], e1.post()[1] + e2.post()[1])
+        else:
+            if type(e1.post()) == int and type(e2.post()) == int:
+                # print('here int')
+                # print(e1.post(), e2.post())
+                return e1.post() + e2.post()
+            return 'Error' 
+        # return 'Int' if e1.post() == 'Int' and e2.post() == 'Int' else 'Error'
+    # def Sub(self, e1, e2): return 'Int' if e1.post() == 'Int' and e2.post() == 'Int' else 'Error'
+    # def Mult(self, e1, e2): return 'Int' if e1.post() == 'Int' and e2.post() == 'Int' else 'Error'
+    # def USub(self, e): return 'Int' if e.post() == 'Int' else 'Error'
+
+    # def Tuple(self, es):
+    #     print('in tuple')
+    #     x = Interval(a, b)
+    #     return x
+    def Name(self, id, context=None): 
+        print('id, ctx', id, context)
+        # print('context[id]', context[id])
+        print('id.post', id.post())
+        return id.post()
+    def BinOp(self, e1, e2, context=None): 
+        print('binop', e1.post(), e2.post()) 
+        return e1.post(context) + e2.post(context)
+    # def Call(self, func, args):
+
+
+@IntervalAI
+def test(x:int, theta:int):
+    x = 1
+    theta = 3
+    x1 = x
+    x2 = theta
+    x3 = x1 + x2
+    return x3
+
+print(str(test.IntervalAI))
+
+# @IntervalAI
+# def ai_test(x:Tuple[float, float], theta:Tuple[float, float]):
+#     return x + theta
+# 
+# print(str(ai_test((1, 2), (3, 3)).IntervalAI))
+
+# @Ty
+# def correct():
+#     return -1 + 2 - 3 * 4
+
+# print("The type of 'correct' is " + str(correct.Ty) + ".")
+
+# @Ty
+# def incorrect():
+#     return 123 and False
+
+# print("The type of 'incorrect' is " + str(incorrect.Ty) + ".")
+
+# @Ty
+# def branch(x):
+#     if x > 0:
+#         return int(x)
+#     else:
+#         return str(x)
+
+# print("The type of 'branch' is " + str(branch.Ty) + ".")
