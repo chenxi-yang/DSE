b7d2c2844c3fcabc38878c12cf480441c2767e3d
cxyang <cxyang@cs.utexas.edu>
2021-06-18 23:36:20 -0500

    update diffAI

--- gpu_framework/args.py
+++ gpu_framework/args.py
@@ -35,7 +35,7 @@ def get_parser():
     
     p.add_argument("--nn_mode", default='all', help="how many NN used in model, 'single' means only used in the first one")
     p.add_argument("--l", default=10, type=int, help="size of hidden states in NN")
-    p.add_argument("--b", default=1000, type=int, help="range of lambda")
+    p.add_argument("--b", default=100, type=int, help="range of lambda")
     p.add_argument("--module", default="linearrelu", help="module in model")
 
     # dataset
