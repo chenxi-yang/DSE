d1546920bf6cbbbaad786d02d37381b27d5d185c
cxyang <cxyang@cs.utexas.edu>
2021-07-13 18:22:36 -0500

    clean benchmark addition

--- gpu_framework/run.py
+++ gpu_framework/run.py
@@ -15,7 +15,6 @@ import time
 
 from utils import (
     extract_abstract_representation,
-    show_cuda_memory,
     count_parameters,
     append_log,
 )
@@ -97,46 +96,9 @@ if __name__ == "__main__":
             # Run 5 times
             for i in range(5):
                 constants.status = 'train'
-                if constants.benchmark_name == "thermostat":
-                    import benchmarks.thermostat as tm
-                    importlib.reload(tm)
-                    from benchmarks.thermostat import *
-                elif constants.benchmark_name == "mountain_car":
-                    import benchmarks.mountain_car as mc
-                    importlib.reload(mc)
-                    from benchmarks.mountain_car import *
-                elif constants.benchmark_name == "unsmooth_1":
-                    import benchmarks.unsmooth_1 as us
-                    importlib.reload(us)
-                    from benchmarks.unsmooth_1 import *
-                elif constants.benchmark_name == "unsmooth_1_a":
-                    import benchmarks.unsmooth_1_a as usa
-                    importlib.reload(usa)
-                    from benchmarks.unsmooth_1_a import *
-                elif constants.benchmark_name == "unsmooth_1_b":
-                    import benchmarks.unsmooth_1_b as usa
-                    importlib.reload(usa)
-                    from benchmarks.unsmooth_1_b import *
-                elif constants.benchmark_name == "unsmooth_1_c":
-                    import benchmarks.unsmooth_1_c as usc
-                    importlib.reload(usc)
-                    from benchmarks.unsmooth_1_c import *
-                elif constants.benchmark_name == "unsmooth_2_separate":
-                    import benchmarks.unsmooth_2_separate as uss
-                    importlib.reload(uss)
-                    from benchmarks.unsmooth_2_separate import *
-                elif constants.benchmark_name == "unsmooth_2_overall":
-                    import benchmarks.unsmooth_2_overall as uso
-                    importlib.reload(uso)
-                    from benchmarks.unsmooth_2_overall import *
-                elif constants.benchmark_name == "path_explosion":
-                    import benchmarks.path_explosion as pe
-                    importlib.reload(pe)
-                    from benchmarks.path_explosion import *
-                elif constants.benchmark_name == "path_explosion_2":
-                    import benchmarks.path_explosion_2 as pe2
-                    importlib.reload(pe2)
-                    from benchmarks.path_explosion_2 import *
+                import import_hub as hub
+                importlib.reload(hub)
+                from import_hub import *
 
                 if mode == 'DSE':
                     import gpu_DSE.train as gt
--- gpu_framework/tester.py
+++ gpu_framework/tester.py
@@ -5,51 +5,13 @@ from constants import *
 import constants
 import importlib
 
-import domain
 from utils import (
     batch_pair_trajectory
     )
 
-if constants.benchmark_name == "thermostat":
-    import benchmarks.thermostat as tm
-    importlib.reload(tm)
-    from benchmarks.thermostat import *
-elif constants.benchmark_name == "mountain_car":
-    import benchmarks.mountain_car as mc
-    importlib.reload(mc)
-    from benchmarks.mountain_car import *
-elif constants.benchmark_name == "unsmooth_1":
-    import benchmarks.unsmooth_1 as us
-    importlib.reload(us)
-    from benchmarks.unsmooth_1 import *
-elif constants.benchmark_name == "unsmooth_1_a":
-    import benchmarks.unsmooth_1_a as usa
-    importlib.reload(usa)
-    from benchmarks.unsmooth_1_a import *
-elif constants.benchmark_name == "unsmooth_1_b":
-    import benchmarks.unsmooth_1_b as usa
-    importlib.reload(usa)
-    from benchmarks.unsmooth_1_b import *
-elif constants.benchmark_name == "unsmooth_1_c":
-    import benchmarks.unsmooth_1_c as usc
-    importlib.reload(usc)
-    from benchmarks.unsmooth_1_c import *
-elif constants.benchmark_name == "unsmooth_2_separate":
-    import benchmarks.unsmooth_2_separate as uss
-    importlib.reload(uss)
-    from benchmarks.unsmooth_2_separate import *
-elif constants.benchmark_name == "unsmooth_2_overall":
-    import benchmarks.unsmooth_2_overall as uso
-    importlib.reload(uso)
-    from benchmarks.unsmooth_2_overall import *
-elif constants.benchmark_name == "path_explosion":
-    import benchmarks.path_explosion as pe
-    importlib.reload(pe)
-    from benchmarks.path_explosion import *
-elif constants.benchmark_name == "path_explosion_2":
-    import benchmarks.path_explosion_2 as pe2
-    importlib.reload(pe2)
-    from benchmarks.path_explosion_2 import *
+import import_hub as hub
+importlib.reload(hub)
+from import_hub import *
 
 
 def trajectory2points(trajectory_list, bs):
