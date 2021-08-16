bde1d2030ebda83752d736426e51d3e21bfda7b5
cxyang <cxyang@cs.utexas.edu>
2021-08-16 14:37:38 -0700

    fix DSE

--- gpu_framework/domain_utils.py
+++ gpu_framework/domain_utils.py
@@ -18,3 +18,22 @@ def concatenate_states(states1, states2):
         'p_list': states1['p_list'] + states2['p_list'],
     }
     return res_states
+
+
+def concatenate_states_list(states_list):
+    # check needed, cause pre-selected as a states
+    if len(states_list) == 0:
+        return states_list[0]
+
+    res_c, res_delta = torch.cat([states['x'].c for states in states_list], 0), \
+        torch.cat([states['x'].delta for states in states_list])
+    trajectories_list = [states['trajectories'] for states in states_list]
+    idx_list = [states['idx_list'] for states in states_list]
+    p_list = [states['p_list'] for states in states_list]
+    res_states = {
+        'x': domain.Box(res_c, res_delta),
+        'trajectories': sum(trajectories_list, []),
+        'idx_list': sum(idx_list, []),
+        'p_list': sum(p_list, []),
+    }
+    return res_states
